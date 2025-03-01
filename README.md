6920cf5f8d8623cbbfed4780073b5d6f48a8bd35
Siddhesh Desai <siddheshdesai777@gmail.com>
2025-03-01 22:08:07 +0530

    initialising new repo

---
+++ README.md
@@ -0,0 +1,135 @@
+<a name="readme-top"></a>
+
+<!-- PROJECT LOGO -->
+<br/>
+<div align="center">
+
+  <h3 align="center"><b>Youtube Comment Classifier</b></h3>
+
+  <p align="center">
+    Youtube Comment Classifier is a platform designed to classify youtube comments based on the user's need.
+    <br />
+    <br>
+    <a href="https://github.com/siddhesh-desai/YoutubeCommentClassifier"><strong>Explore the docs »</strong></a>
+    <br />
+    <a href="https://drive.google.com/file/d/1Jdsm__ofXirzdk_5m5DDQ2T1fb1vaTTl/view">View Demo</a>
+    ·
+    <a href="https://github.com/siddhesh-desai/YoutubeCommentClassifier/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/siddhesh-desai/YoutubeCommentClassifier/issues">Request Feature</a>
+  </p>
+</div>
+
+<!-- ABOUT THE PROJECT -->
+<br>
+
+## ♾️ About The Projects
+
+YouTube comment classifier project is aimed to analyze and categorize comments on YouTube videos, using a combination of sentiment analysis and a Multinomial Naive Bayes Classifier. This project was developed using Flask, a Python web framework, and integrated with the Google YouTube API to fetch comments from YouTube videos. The primary objective of this project was to efficiently filter and categorize comments into five main categories: spam, relevant, appreciation, grievance and all comments.
+
+Integrated with user authentication, the user has to first register with his/her username and password, and then login to the application. While on the application, the user can logout anytime. Visualisation of the comparison between tyoe of comments - (relevant vs spam) and (appreciation vs grievance) as piecharts is provided on the dashboard in order to make the application more insightful and engaging.
+
+The first component of the project involved sentiment analysis, which was used to determine the emotional tone of the comments. This analysis helped identify whether a comment was appreciative or critical, providing valuable insights into the overall sentiment of the viewers. Sentiment analysis used natural language processing techniques to assign a sentiment score to each comment.
+
+The second part of the project utilized a Multinomial Naive Bayes Classifier to distinguish between relevant and spam comments. This classifier was designed to filter out comments that were unrelated or irrelevant to the video's content, ensuring that only meaningful and contextually appropriate comments were displayed.
+
+By combining these two models, the project provided a comprehensive solution for YouTube content creators and viewers to better manage and engage with comments on their videos. The Flask-based web application allowed users to easily input a YouTube video URL and receive categorized comments, helping them quickly identify appreciative or critical feedback and separate relevant comments from spam. This project has the potential to improve user experience and engagement on YouTube, making it a valuable tool for content creators and viewers alike.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## ⚙️ Built With
+
+The technologies and tools used are:
+
+- Flask
+- HTML
+- CSS
+- Python
+- Google Charts
+- Postgres
+- Google Colab
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- GETTING STARTED -->
+
+## 🧑‍💻 Getting Started
+
+Follow the below steps to set up the project locally:
+
+### Installation
+
+1. Clone the repository:
+
+   ```sh
+   git clone https://github.com/siddhesh-desai/YoutubeCommentClassifier.git
+   ```
+
+2. Change the directory:
+
+   ```sh
+   cd YoutubeCommentClassifier
+   ```
+
+3. Install all requirements:
+
+   ```sh
+   pip install -r requirements.txt
+   ```
+
+4. Setup .env file:
+
+   ```python
+   DEVELOPER_KEY="ENTER_DEVELOPER_KEY_HERE"
+   DATABASE_URL="sqlite:///your_database.db"
+   ```
+
+   You need to create your developer key from your GCP console.
+
+5. Run wsgi.py file:
+
+   ```sh
+   python wsgi.py
+   ```
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## 💡 Features
+
+<br>
+
+- Relevant and Spam Comments Detection
+- Appreciation and Resentful Comments Detection
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- ROADMAP -->
+
+## 🛣️ Roadmap
+
+- [x] Enter the Youtube video ID
+- [x] Choose your required button
+- [x] View Comments
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- CONTRIBUTING -->
+
+## 👣 Contributing
+
+Any contributions you make are **greatly appreciated**.
+
+If you have a suggestion that would make this better, please fork the repo and create a pull request. You can also simply open an issue with the tag "enhancement".
+Don't forget to give the project a star! Thanks again!
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+Project Link: [https://github.com/siddhesh-desai/YoutubeCommentClassifier](https://github.com/siddhesh-desai/YoutubeCommentClassifier)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
---
+++ app.py
@@ -0,0 +1,243 @@
+from flask import Flask, render_template, request, url_for, redirect
+from googleapiclient.discovery import build
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
+import numpy as np
+import pickle
+import os
+
+load_dotenv()
+
+# Generate a secure random secret key
+secret_key = os.urandom(24)
+
+# Convert the bytes to a string for easy use in Flask
+secret_key_str = secret_key.hex()
+
+# Creating Flask Server
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SECRET_KEY'] = secret_key_str
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(200), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)])
+
+    password = PasswordField(
+        validators=[InputRequired(), Length(min=8, max=20)])
+
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                'Username already exists.')
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)])
+
+    password = PasswordField(
+        validators=[InputRequired(), Length(min=8, max=20)])
+
+    submit = SubmitField('Login')
+
+
+with app.app_context():
+    db.create_all()
+
+with open('models/spam_detection_model.pkl', 'rb') as model_file:
+    spam_detection_model = pickle.load(model_file)
+
+with open('models/sentiment_analysis_model.pkl', 'rb') as model_file:
+    sentiment_analysis_model = pickle.load(model_file)
+
+with open('models/vectorizer.pkl', 'rb') as vectorizer_file:
+    tfidf_vectorizer = pickle.load(vectorizer_file)
+
+# Function to extract youtube comments from ID
+yt_link = ""
+
+
+def get_youtube_comments(video_id):
+    youtube = build('youtube', 'v3', developerKey=os.getenv('DEVELOPER_KEY'))
+    comments = []
+    counter = 10
+
+    next_page_token = None
+    while True:
+        if counter == 0:
+            break
+        else:
+            counter = counter - 1
+
+        results = youtube.commentThreads().list(
+            part="snippet",
+            videoId=video_id,
+            textFormat="plainText",
+            maxResults=100,
+            pageToken=next_page_token
+        ).execute()
+
+        for item in results.get("items", []):
+            comment = item["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
+            comments.append(comment)
+
+        # Check if there are more comments
+        next_page_token = results.get("nextPageToken")
+        if not next_page_token:
+            break
+
+    return comments
+
+
+def classify_count_comments(comments):
+
+    global all_comments, relevant_comments, spam_comments, good_comments, bad_comments
+
+    all_comments = np.array(comments)
+    cnt1 = len(all_comments)
+
+    # Array for relevant and spam comments
+    label_col = []
+    cp = []
+    for c in all_comments:
+        cp.append([c])
+        comment_features = [c]
+        features = np.array(comment_features)
+        data_tfidf = tfidf_vectorizer.transform(features)
+        prediction = spam_detection_model.predict(data_tfidf)
+        label_col.append(prediction)
+
+    label_array = np.array(label_col)
+    relevant_comments = np.array(cp)[label_array == 0]
+    spam_comments = np.array(cp)[label_array == 1]
+    cnt2 = len(relevant_comments)
+    cnt3 = len(spam_comments)
+
+    # Array for comments of appreciation and griviences
+    label_col = []
+    cp = []
+    for c in all_comments:
+        cp.append(c)
+        prediction_dict = sentiment_analysis_model.polarity_scores(c)
+        prediction = prediction_dict["compound"]
+        label_col.append(prediction)
+
+    label_array = np.array(label_col)
+    good_comments = np.array(cp)[label_array >= 0.5]
+    bad_comments = np.array(cp)[label_array <= -0.5]
+    cnt4 = len(good_comments)
+    cnt5 = len(bad_comments)
+    cnt_array = [cnt1, cnt2, cnt3, cnt4, cnt5]
+
+    return cnt_array
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+    return render_template('login.html', form=form)
+
+
+@ app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return render_template('home.html')
+
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('index.html')
+
+
+@app.route('/process_link', methods=['POST'])
+@login_required
+def process_link():
+    youtube_link = request.form.get('youtube_link')
+    global yt_link
+    yt_link = youtube_link
+    comments = get_youtube_comments(video_id=yt_link)
+    cnt_array = []
+    cnt_array = classify_count_comments(comments)
+
+    return render_template('class.html', cnt_array=cnt_array)
+
+
+@app.route('/all', methods=['POST'])
+@login_required
+def all():
+    button_type = request.form['button_name']
+    global all_comments, relevant_comments, spam_comments, good_comments, bad_comments
+    if (button_type == 'All'):
+        required_comments = all_comments
+
+    elif (button_type == 'Relevant'):
+        required_comments = relevant_comments
+
+    elif (button_type == 'Spam'):
+        required_comments = spam_comments
+
+    elif (button_type == 'Appreciation'):
+        required_comments = good_comments
+
+    elif (button_type == 'Grievance'):
+        required_comments = bad_comments
+
+    return render_template('comments-page.html', comments=required_comments, type=button_type)
+
+# For trial, use the ID of Eminem song video : 2BeksVabsCk
